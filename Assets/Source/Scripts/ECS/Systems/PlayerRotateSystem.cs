f226c160513922133aeb1d55f478cf100be9f2f2
sulphur34 <toku_gawa@mail.ru>
2024-09-06 18:56:53 +0300

    Change hover pivots

--- Assets/Source/Scripts/ECS/Systems/PlayerRotateSystem.cs
+++ Assets/Source/Scripts/ECS/Systems/PlayerRotateSystem.cs
@@ -15,7 +15,7 @@ namespace Systems
                 ref var rotatableComponent = ref _playerMoveFilter.Get1(entity);
                 ref var inputComponent = ref _playerMoveFilter.Get2(entity);
 
-                rotatableComponent.transform.right = new Vector3(inputComponent.direction.x, inputComponent.direction.y, 0f);
+                rotatableComponent.transform.right = new Vector3(inputComponent.direction.y, -inputComponent.direction.x, 0f);
             }
         }
     }
