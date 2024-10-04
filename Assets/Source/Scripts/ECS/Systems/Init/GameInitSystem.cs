67f64f7844e250023d7da7685a0df8375b3cf533
sulphur34 <toku_gawa@mail.ru>
2024-10-04 14:00:27 +0300

    Create unit builder

--- Assets/Source/Scripts/ECS/Systems/Init/GameInitSystem.cs
+++ Assets/Source/Scripts/ECS/Systems/Init/GameInitSystem.cs
@@ -43,7 +43,8 @@ namespace Systems
 
         public void Init()
         {
-            var playerActor = CreatePlayer();
+            var unitBuilder = new UnitBuilder(_world);
+            var playerActor = unitBuilder.BuildPlayer(_playerInitData, _spawnPoint.position);
             foreach (var weaponPlace in playerActor.FrontWeaponsPlaceholders)
             {
                 var weapon = CreateWeapon(
@@ -52,7 +53,6 @@ namespace Systems
                     weaponPlace,
                     weaponPlace.position);
                 weapon.Get<ShootInputComponent>();
-
             }
 
             CreateTurret(playerActor);
@@ -61,7 +61,7 @@ namespace Systems
             {
                 var enemySpawnPosition = _spawnPoint.position +
                                          new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), 0f);
-                CreateEnemy(enemySpawnPosition, playerActor.transform);
+                unitBuilder.BuildEnemy(_enemyInitData, enemySpawnPosition, playerActor.transform);
                 CreatePickUps();
             }
         }
@@ -119,13 +119,15 @@ namespace Systems
                 turretComponent.weapons = new List<EcsEntity>();
                 foreach (var position in turretActor.WeaponPositions)
                 {
-                    var weapon = CreateWeapon(_turretWeaponInitData,turretActor.WeaponActor, turretActor.transform, position);
+                    var weapon = CreateWeapon(_turretWeaponInitData, turretActor.WeaponActor, turretActor.transform,
+                        position);
                     turretComponent.weapons.Add(weapon);
                 }
             }
         }
 
-        private EcsEntity CreateWeapon(WeaponInitData weaponInitData, WeaponActor weaponPrefab, Transform container, Vector2 position)
+        private EcsEntity CreateWeapon(WeaponInitData weaponInitData, WeaponActor weaponPrefab, Transform container,
+            Vector2 position)
         {
             var weaponActor = Object.Instantiate(weaponPrefab, container);
             weaponActor.transform.position = position;
@@ -144,106 +146,6 @@ namespace Systems
             ammoComponent.currentCapacity = ammoComponent.maxCapacity;
 
             return weapon;
-        }
-
-        private UnitActor CreatePlayer()
-        {
-            var unitActor = Object.Instantiate(_playerInitData.UnitPrefab, _spawnPoint.position, _spawnPoint.rotation);
-            var player = _world.NewEntity();
-
-            RemoveCollidersInteractions(unitActor.InternalColliders);
-
-            player.Get<PlayerComponent>();
-            player.Get<RotationInputEventComponent>();
-            player.Get<MoveInputEventComponent>();
-            player.Get<PlayerComponent>();
-            player.Get<CollisionDestructionComponent>();
-
-            ref var cameraComponent = ref player.Get<CameraComponent>();
-            cameraComponent.camera = Camera.main;
-            cameraComponent.defaultPosition = Camera.main.transform.position;
-            cameraComponent.distanceRate = 2f;
-
-            ref var targetableComponent = ref player.Get<TargetableComponent>();
-            targetableComponent.transform = unitActor.transform;
-            targetableComponent.team = Teams.Player;
-
-            var colliderObserver = unitActor.GetComponent<ColliderObserver>();
-            colliderObserver.Initialize(_world, player);
-            var rigidbody = unitActor.Rigidbody2D;
-
-            ref var movableComponent = ref player.Get<RigidbodyMovableComponent>();
-            movableComponent.movingData = _playerInitData.MovingData;
-            movableComponent.rigidbody = rigidbody;
-            movableComponent.transform = unitActor.transform;
-
-            ref var rotatableComponent = ref player.Get<RigidbodyRotatableComponent>();
-            rotatableComponent.rigidbody = rigidbody;
-            rotatableComponent.rotationData = _playerInitData.RotationData;
-
-            ref var animationsComponent = ref player.Get<AnimatedCharacterComponent>();
-            animationsComponent.animator = unitActor.Animator;
-
-            ref var moveParticleComponent = ref player.Get<MoveParticleComponent>();
-            moveParticleComponent.particleSystem = unitActor.MoveParticleSystem;
-
-            ref var collisionParticleComponent = ref player.Get<SelfCollisionParticleComponent>();
-            collisionParticleComponent.particleSystem = unitActor.CollisionParticleSystem;
-
-            ref var healthComponent = ref player.Get<HealthComponent>();
-            healthComponent.maxValue = _playerInitData.HealthValue;
-            healthComponent.currentValue = _playerInitData.HealthValue;
-
-            return unitActor;
-        }
-
-        private void CreateEnemy(Vector3 atPosition, Transform target)
-        {
-            var unitActor = Object.Instantiate(_enemyInitData.UnitPrefab, atPosition, Quaternion.identity);
-
-            var enemy = _world.NewEntity();
-
-            RemoveCollidersInteractions(unitActor.InternalColliders);
-
-            enemy.Get<CollisionDestructionComponent>();
-
-            ref var targetableComponent = ref enemy.Get<TargetableComponent>();
-            targetableComponent.transform = unitActor.transform;
-            targetableComponent.team = Teams.Enemy;
-
-            var colliderObserver = unitActor.GetComponent<ColliderObserver>();
-            colliderObserver.Initialize(_world, enemy);
-
-            ref var enemyMovableComponent = ref enemy.Get<MovableComponent>();
-            enemyMovableComponent.moveSpeed = _enemyInitData.DefaultSpeed;
-            enemyMovableComponent.transform = unitActor.transform;
-
-            ref var enemyRotatableComponent = ref enemy.Get<RotatableComponent>();
-            enemyRotatableComponent.transform = unitActor.transform;
-
-            ref var enemyAnimationsComponent = ref enemy.Get<AnimatedCharacterComponent>();
-            enemyAnimationsComponent.animator = unitActor.Animator;
-
-            ref var followComponent = ref enemy.Get<FollowComponent>();
-            followComponent.target = target;
-
-            ref var enemyMoveParticleComponent = ref enemy.Get<MoveParticleComponent>();
-            enemyMoveParticleComponent.particleSystem = unitActor.MoveParticleSystem;
-
-            ref var healthComponent = ref enemy.Get<HealthComponent>();
-            healthComponent.maxValue = _enemyInitData.HealthValue;
-            healthComponent.currentValue = _enemyInitData.HealthValue;
-        }
-
-        private void RemoveCollidersInteractions(Collider2D[] colliders)
-        {
-            for (int i = 0; i < colliders.Length; i++)
-            {
-                for (int j = 0; j < colliders.Length; j++)
-                {
-                    Physics2D.IgnoreCollision(colliders[i], colliders[j]);
-                }
-            }
         }
     }
 }
